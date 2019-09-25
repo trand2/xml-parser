5c560c6400c229d22b32ee3b17d554402381ab94
TRand2 <tylerrandolph123@gmail.com>
2019-09-24 21:28:26 -0600

    Initial Commit

---
+++ XML-Parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XML-Parser
+//
+//  Created by Tyler Randolph on 9/24/19.
+//  Copyright © 2019 Tyler Randolph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
