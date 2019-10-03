b50ed022295bc8663951881281e5951a54de2850
TRand2 <tylerrandolph123@gmail.com>
2019-10-03 13:20:27 -0600

    first commit

---
+++ XML-Parser/Employee.cpp
@@ -0,0 +1,9 @@
+//
+//  Employee.cpp
+//  XML-Parser
+//
+//  Created by Tyler Randolph on 9/24/19.
+//  Copyright © 2019 Tyler Randolph. All rights reserved.
+//
+
+#include "Employee.hpp"
---
+++ XML-Parser/Employee.hpp
@@ -0,0 +1,14 @@
+//
+//  Employee.hpp
+//  XML-Parser
+//
+//  Created by Tyler Randolph on 9/24/19.
+//  Copyright © 2019 Tyler Randolph. All rights reserved.
+//
+
+#ifndef Employee_hpp
+#define Employee_hpp
+
+#include <stdio.h>
+
+#endif /* Employee_hpp */
